67a63b591e3dda66e0b59d8041faf7172b18ffb1
Kavita-LachmanDas <kavitaluhana11@gmail.com>
2024-12-08 18:09:50 +0500

    hackathon

--- src/app/Card.tsx
+++ src/app/Card.tsx
@@ -1,18 +1,44 @@
-import { StaticImport } from 'next/dist/shared/lib/get-img-props'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+// import { StaticImport } from 'next/dist/shared/lib/get-img-props'
+// import Image from 'next/image'
+// import Link from 'next/link'
+// import React from 'react'
 
-export default function Card(Props: { img: string | StaticImport; name: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; veiw: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined }) {
+// export default function Card(Props: { img: string | StaticImport; name: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; veiw: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined }) {
+//   return (
+//     <div className="w-full sm:w-[350px] md:w-[400px] lg:w-[450px] px-4 py-6">
+//         <div className="img">
+//             <Image src={Props.img} alt='hi' width={700} height={500} />
+//         </div>
+//         <div className="text mb-11">
+//             <h1 className='font-bold text-3xl'>{Props.name}</h1>
+//             <Link className='underline' href={'#'}>{Props.veiw}</Link>
+//         </div>
+//     </div>
+//   )
+// }
+import { StaticImport } from 'next/dist/shared/lib/get-img-props';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+interface CardProps {
+  img: string | StaticImport; // The image source can be a string or a StaticImport
+  name: string | React.ReactNode; // Text or React-compatible content for the name
+  veiw: string | React.ReactNode; // Text or React-compatible content for the view
+}
+
+export default function Card({ img, name, veiw }: CardProps) {
   return (
     <div className="w-full sm:w-[350px] md:w-[400px] lg:w-[450px] px-4 py-6">
-        <div className="img">
-            <Image src={Props.img} alt='hi' width={700} height={500} />
-        </div>
-        <div className="text mb-11">
-            <h1 className='font-bold text-3xl'>{Props.name}</h1>
-            <Link className='underline' href={'#'}>{Props.veiw}</Link>
-        </div>
+      <div className="img">
+        <Image src={img} alt="hi" width={700} height={500} />
+      </div>
+      <div className="text mb-11">
+        <h1 className="font-bold text-3xl">{name}</h1>
+        <Link className="underline" href={'#'}>
+          {veiw}
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
--- src/app/component/Card3.tsx
+++ src/app/component/Card3.tsx
@@ -1,23 +1,59 @@
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
-import { BiCalendar, BiTime } from 'react-icons/bi'
-import { StaticImport } from 'next/dist/shared/lib/get-img-props'
+// import React from 'react'
+// import Image from 'next/image'
+// import Link from 'next/link'
+// import { BiCalendar, BiTime } from 'react-icons/bi'
+// import { StaticImport } from 'next/dist/shared/lib/get-img-props'
 
-export default function Card3(Props: { img: string | StaticImport; para: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined }) {
+// export default function Card3(Props: { img: string | StaticImport; para: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined }) {
+//   return (
+//     <div className="w-full sm:w-[350px] md:w-[300px] lg:w-[350px] p-4 overflow-x-hidden">
+//       <div className="img">
+//         <Image
+//           width={300}
+//           height={300}
+//           src={Props.img} // Place your image in the public folder
+//           alt="Rocket single seater"
+//           className="w-full max-w-xs md:max-w-md lg:max-w-full object-contain"
+//         />
+//       </div>
+//       <div className="text mt-4">
+//         <p className="text-center">{Props.para}</p>
+//         <Link className="underline block text-center" href={'#'}>Read More</Link>
+//         <div className="flex items-center justify-center mt-2 text-sm">
+//           <h1 className="flex items-center space-x-2">
+//             <BiTime /> <span>5 min</span>
+//             <BiCalendar /> <span>12<sup>th</sup> Oct 2022</span>
+//           </h1>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { BiCalendar, BiTime } from 'react-icons/bi';
+import { StaticImport } from 'next/dist/shared/lib/get-img-props';
+
+interface CardProps {
+  img: string | StaticImport;
+  para: string | number | React.ReactNode; // Using React.ReactNode for diverse React elements
+}
+
+export default function Card3({ img, para }: CardProps) {
   return (
     <div className="w-full sm:w-[350px] md:w-[300px] lg:w-[350px] p-4 overflow-x-hidden">
       <div className="img">
         <Image
           width={300}
           height={300}
-          src={Props.img} // Place your image in the public folder
+          src={img} // Place your image in the public folder
           alt="Rocket single seater"
           className="w-full max-w-xs md:max-w-md lg:max-w-full object-contain"
         />
       </div>
       <div className="text mt-4">
-        <p className="text-center">{Props.para}</p>
+        <p className="text-center">{para}</p>
         <Link className="underline block text-center" href={'#'}>Read More</Link>
         <div className="flex items-center justify-center mt-2 text-sm">
           <h1 className="flex items-center space-x-2">
@@ -27,5 +63,5 @@ export default function Card3(Props: { img: string | StaticImport; para: string 
         </div>
       </div>
     </div>
-  )
+  );
 }
